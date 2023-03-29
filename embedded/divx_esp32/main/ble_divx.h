1f7dff0dc0a49b63be27a1c068e48be50afa7785
Saviour <owolabisaviour@gmail.com>
2023-03-30 00:04:51 +0100

    Add function to set xcode characteristic value

--- embedded/divx_esp32/main/ble_divx.h
+++ embedded/divx_esp32/main/ble_divx.h
@@ -9,6 +9,11 @@
  * 
  */
 
+#ifndef x_HEADER_BLE
+
+#define x_HEADER_BLE
+
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
@@ -147,7 +152,8 @@ static const uint8_t char_prop_write               = ESP_GATT_CHAR_PROP_BIT_WRIT
 static const uint8_t char_prop_read_write_notify   = ESP_GATT_CHAR_PROP_BIT_WRITE | ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_NOTIFY;
 
 static const uint8_t char_xcode_ccc[2]  =     {0x00, 0x00};
-static const uint8_t char_value[4]      =     {0x11, 0x22, 0x33, 0x44};
+// static const uint8_t char_value[4]      =     {0x11, 0x22, 0x33, 0x44};
+uint8_t xcode_char_value[16]      =     {0x11, 0x22, 0x33, 0x44};
 
 
 /* Full Database Description - Used to add attributes into the database */
@@ -166,7 +172,7 @@ static const esp_gatts_attr_db_t gatt_db[DIVX_IDX_NB] =
     /* Characteristic Value */
     [DIVX_IDX_XCODE_VAL] =
     {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&GATTS_CHAR_UUID_XCODE, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
-      DIVX_CHAR_VAL_LEN_MAX, sizeof(char_value), (uint8_t *)char_value}},
+      DIVX_CHAR_VAL_LEN_MAX, sizeof(xcode_char_value), (uint8_t *)xcode_char_value}},
 
     /* Client Characteristic Configuration Descriptor */
     [DIVX_IDX_XCODE_NTF_CFG]  =
@@ -378,65 +384,10 @@ static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_
     } while (0);
 }
 
-// void app_main(void)
-// {
-//     esp_err_t ret;
 
-//     /* Initialize NVS. */
-//     ret = nvs_flash_init();
-//     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-//         ESP_ERROR_CHECK(nvs_flash_erase());
-//         ret = nvs_flash_init();
-//     }
-//     ESP_ERROR_CHECK( ret );
+esp_err_t set_xcode_value(uint8_t *xvalue){
+    esp_err_t ret = esp_ble_gatts_set_attr_value(divx_handle_table[DIVX_IDX_XCODE_VAL], sizeof(xcode_char_value), xvalue); 
+    return ret;
+}
 
-//     ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
-
-//     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
-//     ret = esp_bt_controller_init(&bt_cfg);
-//     if (ret) {
-//         ESP_LOGE(DIVX_BLE_TAG, "%s enable controller failed: %s", __func__, esp_err_to_name(ret));
-//         return;
-//     }
-
-//     ret = esp_bt_controller_enable(ESP_BT_MODE_BLE);
-//     if (ret) {
-//         ESP_LOGE(DIVX_BLE_TAG, "%s enable controller failed: %s", __func__, esp_err_to_name(ret));
-//         return;
-//     }
-
-//     ret = esp_bluedroid_init();
-//     if (ret) {
-//         ESP_LOGE(DIVX_BLE_TAG, "%s init bluetooth failed: %s", __func__, esp_err_to_name(ret));
-//         return;
-//     }
-
-//     ret = esp_bluedroid_enable();
-//     if (ret) {
-//         ESP_LOGE(DIVX_BLE_TAG, "%s enable bluetooth failed: %s", __func__, esp_err_to_name(ret));
-//         return;
-//     }
-
-//     ret = esp_ble_gatts_register_callback(gatts_event_handler);
-//     if (ret){
-//         ESP_LOGE(DIVX_BLE_TAG, "gatts register error, error code = %x", ret);
-//         return;
-//     }
-
-//     ret = esp_ble_gap_register_callback(gap_event_handler);
-//     if (ret){
-//         ESP_LOGE(DIVX_BLE_TAG, "gap register error, error code = %x", ret);
-//         return;
-//     }
-
-//     ret = esp_ble_gatts_app_register(DIVX_APP_ID);
-//     if (ret){
-//         ESP_LOGE(DIVX_BLE_TAG, "gatts app register error, error code = %x", ret);
-//         return;
-//     }
-
-//     esp_err_t local_mtu_ret = esp_ble_gatt_set_local_mtu(500);
-//     if (local_mtu_ret){
-//         ESP_LOGE(DIVX_BLE_TAG, "set local  MTU failed, error code = %x", local_mtu_ret);
-//     }
-// }
+#endif
