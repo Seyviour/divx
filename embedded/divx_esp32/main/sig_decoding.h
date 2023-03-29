4fbdb53e9be6d080165a4d3da3763b39ea47bf01
Saviour <owolabisaviour@gmail.com>
2023-03-29 14:22:12 +0100

    Refactor code resolution logic, add ERROR state

--- embedded/divx_esp32/main/sig_decoding.h
+++ embedded/divx_esp32/main/sig_decoding.h
@@ -6,8 +6,8 @@
 / 16 is significant
 */
 
-#define x_HISTORY_SIZE 32
-#define x_CADENCE 768
+#define x_HISTORY_SIZE 16
+#define x_CADENCE 256
 #define x_SHIFT_FACTOR 3 
 #define CAPTURE_WINDOW_SIZE 128
 #define BIN_WINDOW_SIZE 8
@@ -16,13 +16,22 @@
 #define x_SIG_THRESHOLD 300
 #define x_type uint8_t
 
+#define DECLARATION_WIDTH 4
+
+
+uint8_t abs_diff(uint8_t a, uint8_t b){
+    return (a > b? (a-b): (b-a)); 
+}
+
+char LETTERS[4] = {'A', 'B', 'C', 'D'}; 
 
 enum decode_state {
     IDLE,
     DECLARATION,
     MESSAGE, 
-    TERMINATION,   
-} ; 
+    TERMINATION,
+    ERROR 
+}; 
 
 typedef struct {
     uint8_t start;
@@ -65,7 +74,7 @@ uint8_t pickBins(decode_frame_t *x_df);
 // void incrementalClear(decode_frame_t *x_df);
 void x_reset(decode_frame_t*, uint8_t, uint8_t, uint8_t);
 void x_resolve_reset(decode_frame_t *x_df);
-void x_determine(decode_frame_t *x_df, uint8_t resolve_idx);
+void x_determine(decode_frame_t *x_df);
 
 
 void incrementalDecode(decode_frame_t *x_df, uint8_t *adc_stream, uint32_t ret_num){
@@ -84,7 +93,7 @@ void incrementalDecode(decode_frame_t *x_df, uint8_t *adc_stream, uint32_t ret_n
         // uint16_t bin = reading >> 4; 
 
         //NAIVE OVERFLOW HANDLING
-        if (x_df->decode_bins[bin]<UINT16_MAX)
+        if (x_df->decode_bins[bin]<512)
             x_df->decode_bins[bin] += 1;
 
         x_df->frame_count += 1;
@@ -106,37 +115,59 @@ uint8_t pickBins(decode_frame_t *x_df){
         printf("%04"PRIu16" ", x_df->decode_bins[j]); 
     }
 
+    // NAIVELY COMPUTE AVERAGE
+    // A BETTER IMPLEMENTATION WOULD TAKE ADVANTAGE OF INFORMATION FROM ```incrementalDecode```
+
+    uint32_t bin_ave = 0;
+    for (int j=0; j<256; j++){
+        bin_ave += x_df->decode_bins[j]; 
+    }
+
+    printf("BIN AVERAGE %"PRIu32, bin_ave);
+    bin_ave = bin_ave >> 8; 
+     
     // return 0; 
 
-    // x_df->detect_patterns[0].valid = false;
-    // x_df->detect_patterns[1].valid = false; 
+    x_df->detect_patterns[0].valid = false;
+    x_df->detect_patterns[1].valid = false; 
     // return 0; 
-
+    
+    bool detect = false; 
     uint8_t sig_seen = 0; 
-    for (int i = 0; i < NUMBER_DECODE_BINS; i++){
-        if (x_df -> decode_bins[i] >= x_SIG_THRESHOLD) {
-            x_df -> detect_patterns[sig_seen].start = i;
-            x_df -> detect_patterns[sig_seen].max = i;
-            x_df -> detect_patterns[sig_seen].valid = true; 
-            i++;
-            while (x_df->decode_bins[i] >= x_SIG_THRESHOLD>>3) {
-                i++;
-                if (x_df->decode_bins[i]>x_df->decode_bins[x_df->detect_patterns[sig_seen].max])
-                    x_df->detect_patterns[sig_seen].max = i;
-            };
-            --i;
-            x_df->detect_patterns[sig_seen].end = i; 
-            sig_seen += 1; 
+    uint16_t bin3sum = x_df->decode_bins[0] + x_df->decode_bins[1];
+    uint16_t max3sum = 0;
+    uint16_t sub = 0;
+    uint16_t SOME_THRESHOLD = bin_ave +  (bin_ave << 4); 
+
+    for (int i = 1; i < NUMBER_DECODE_BINS-2; i++){
+        bin3sum += x_df->decode_bins[i+1] - sub;
+
+        if (detect && bin3sum<=x_df->detect_patterns[sig_seen].max >> 2){
+            x_df->detect_patterns[sig_seen].end = i-1;
+            sig_seen += 1;
+            detect = false;
         }
+
+        else if (bin3sum >= SOME_THRESHOLD && !detect){
+            detect = true;
+            x_df->detect_patterns[sig_seen].max = i;
+            x_df->detect_patterns[sig_seen].valid = true;
+            x_df->detect_patterns[sig_seen].start = i-1;
+            max3sum = bin3sum; 
+        }
+
+        else if (detect && bin3sum>max3sum){
+            max3sum = bin3sum; 
+            x_df->detect_patterns[sig_seen].max = i;
+        }
+
+        sub = x_df->decode_bins[i-1]; 
+    
     }
 
     return sig_seen; 
 }
 
-
-uint8_t abs_diff(uint8_t a, uint8_t b){
-    return (a > b? (a-b): (b-a)); 
-}
 
 void x_resolve_reset(decode_frame_t *x_df){
     ESP_LOGI("INFO", "CALLING x_resolve_reset");
@@ -166,73 +197,102 @@ void x_resolve_reset(decode_frame_t *x_df){
         ESP_LOGI("DETECTION EVENT", "EMIT %"PRIu8" %"PRIu8" %"PRIu8, x_df->detect_patterns[emit].start, x_df->detect_patterns[emit].max, x_df->detect_patterns[emit].end); 
         ESP_LOGI("DETECTION EVENT", "PRESERVE ""%"PRIu8" %"PRIu8" %"PRIu8, x_df->detect_patterns[retain].start, x_df->detect_patterns[retain].max, x_df->detect_patterns[retain].end); 
         x_df->old_pattern = x_df->detect_patterns[retain];
-        x_determine(x_df, emit);
+        x_df->emit_history[x_df->num_emits] = x_df->detect_patterns[emit].max; 
+        x_df->num_emits += 1; 
+        x_determine(x_df);
         x_reset(x_df, 1, x_df->detect_patterns[retain].start, x_df->detect_patterns[retain].end);
                        
     }
 }
 
-void x_determine(decode_frame_t *x_df, uint8_t resolve_idx){
-
-    if (x_df->num_emits >= 32){
-        ESP_LOGI("DEBUG", "Exceeded buffer size for decoding"); 
-    }
-
-    const detect_pattern_t current_pattern = x_df->detect_patterns[resolve_idx];
-
-    if (x_df->decode_state != MESSAGE && x_df->num_emits>=3 && current_pattern.max < x_df->emit_history[x_df->num_emits-1]){
-        x_df->decode_state = MESSAGE; 
-    }
-
-    uint16_t min_dist = UINT16_MAX;
-    uint16_t curr_dist; 
-    unsigned char letter = '.';
-
+void x_determine(decode_frame_t *x_df){
+    if (x_df -> num_emits >= 32) {
+		ESP_LOGI("DEBUG", "Exceeded decode buffer size");
+		x_df->decode_state = TERMINATION;
+	}
+	
+	if (x_df->num_emits == 1){
+		x_df->decode_history[0] = 'A';
+		return; 
+	}
+	
+	const uint8_t current_emit = x_df->emit_history[x_df->num_emits-1];
+	const uint8_t prev_emit = x_df->emit_history[x_df->num_emits-2];  
+	
+	if (x_df->decode_state == DECLARATION){
+		if (x_df->num_emits > DECLARATION_WIDTH){
+			if(current_emit < prev_emit){
+				x_df->decode_state = MESSAGE;
+			}
+		
+		} else{
+			if (current_emit < prev_emit){
+				ESP_LOGI("DECODER->x_determine", "Decode Error: Invalid declatation region");
+				x_df->decode_state = ERROR;
+			} else{
+				x_df->decode_history[x_df->num_emits-1] = LETTERS[x_df->num_emits-1];  
+			}
+				
+		}
+	}
+	
+	
+	if (x_df->decode_state == MESSAGE){
+		uint16_t min_dist = UINT16_MAX;
+		uint16_t this_dist = UINT16_MAX;
+		int i = 0;
+		for (i = 0; i < DECLARATION_WIDTH; i++){
+			this_dist = abs_diff(current_emit, x_df->emit_history[i]); 
+			if (this_dist < min_dist){
+				x_df->decode_history[x_df->num_emits-1]=LETTERS[i];
+				min_dist = this_dist; 
+			}		
+		}
+		
+		if (i == 0 && x_df->decode_history[x_df->num_emits-2] == DECLARATION_WIDTH-1){
+			x_df->decode_state = TERMINATION; 
+		}  
+		
+	}
+	
+	if (x_df->decode_state == TERMINATION) {
+		ESP_LOGI("DECODER->x_determine", "Decoding done or in improper state to decode"); 
+	}
     
-
-    if (x_df->decode_state == MESSAGE){
-        for (uint8_t i = 0; i < x_df->num_emits; i++){
-            if ((curr_dist = abs_diff(current_pattern.max, x_df->emit_history[i])) < min_dist){
-                letter = x_df->decode_history[i];
-                min_dist = curr_dist; 
-            }
-        }
-    } else if (x_df->decode_state == DECLARATION) {
-        letter = (unsigned char) ('A' + x_df->num_emits);
-    }
-
-    x_df->decode_history[x_df->num_emits] = letter;
-    
-    x_df->emit_history[x_df->num_emits]=x_df->detect_patterns[resolve_idx].max;
-    x_df->num_emits += 1;
-    
-    ESP_LOGI("DEBUG", "EMIT HISTORY");
-
-    for (uint8_t i = 0; i<12; i++){
-        printf("%04"PRIu8" ", x_df->emit_history[i]);
-        printf("%c", x_df->decode_history[i]);  
-    }
 }
 
 void x_initialize(decode_frame_t *x_df){
 
     // THESE SHOULD BE ARGUMENTS
+    // x_df->decode_state = DECLARATION; 
     memset(x_df->capture_window, 0x00, sizeof(x_df->capture_window[1])*CAPTURE_WINDOW_SIZE);
-    x_df->num_samples = 0; 
-    x_df->capture_window_sum = 0;
-    x_df->capture_window_idx = 0;
     x_df->bin_width = 12;
     x_df->shift_factor = 3;
     x_df->cadence = 256;
     x_df->window_width = 8;
     x_df->threshold = 3;
-    x_df->old_pattern.valid = false;
+}
+
+void x_prepare_decoder(decode_frame_t *x_df){
+    memset(x_df->capture_window, 0x00, sizeof(x_df->capture_window[1])*CAPTURE_WINDOW_SIZE);
+    x_df->num_samples = 0; 
+    x_df->capture_window_sum = 0;
+    x_df->capture_window_idx = 0;
     x_df->num_emits = 0;
     memset(x_df->emit_history, 0, sizeof(x_df->emit_history));
+    x_df->old_pattern.valid = false;
     x_reset(x_df, 1, 255, 254); 
 }
 
 void x_reset(decode_frame_t *x_df, uint8_t reset_type, uint8_t preserve_start, uint8_t preserve_end){
+
+
+    /**
+     * @brief Resets occur after 2 spikes have been detected in the bin array
+     * When that occurs, the area after the newer bin is preserved while the 
+     * rest of the bin array is set to 0
+     * 
+     */
 
     //reset type: 0 - decode_bins is not cleared
     //reset type: 1 - decode_bins is cleared
@@ -249,7 +309,7 @@ void x_reset(decode_frame_t *x_df, uint8_t reset_type, uint8_t preserve_start, u
     }
 
     if (reset_type ==0){
-        x_df->decode_state = IDLE; 
+        x_df->decode_state = DECLARATION; 
     }
 
     x_df->detect_patterns[0].valid = false;
