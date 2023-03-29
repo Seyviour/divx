ce84b2dca658f970cb3df5934791f4965b0b19c4
Saviour <owolabisaviour@gmail.com>
2023-03-29 16:01:56 +0100

    Register interrupt for handling scan_reset

--- embedded/divx_esp32/main/divx_main.c
+++ embedded/divx_esp32/main/divx_main.c
@@ -19,14 +19,12 @@
 #include "ble_divx.h"
 #include "driver/gpio.h"
 
-// static const char *TAG = "EXAMPLE";
-
-#define TAG "EXAMPLE"
 
 
-//DMA BUFFER SIZE 
-#define READ_LEN   256
+#define TAG "DIVX_MAIN"
 
+ 
+#define READ_LEN   256      //DMA BUFFER SIZE
 #define GET_UNIT(x)        ((x>>3) & 0x1)
 
 //ADC RELATED
@@ -34,30 +32,36 @@
 #define ADC_OUTPUT_TYPE     ADC_DIGI_OUTPUT_FORMAT_TYPE1
 static adc_channel_t channel[1] = {ADC_CHANNEL_6};
 
-//
-
-#define NUMBER_DECODE_BINS 256
-#define BIN_HISTORY_SIZE 4
-#define MOVING_AVERAGE_WINDOW_WIDTH 32
-
-static TaskHandle_t s_task_handle;
-
-
-
-
-
+// Push button input pin
 #define SCAN_RESET_PIN GPIO_NUM_35
 
-uint8_t result[READ_LEN] = {0};
+static TaskHandle_t s_task_handle; // main task 
+static TaskHandle_t button_start_task_handle = NULL; // Button decode start task 
 
 
+
+/**
+ * @brief 
+ * 
+ * @param x_df : A decode frame of type decode_frame_t
+ * @param adc_stream : Byte array of adc readings
+ * @param ret_num : number of valid samples in adc_stream
+ */
 void x_decode(decode_frame_t *x_df, uint8_t *adc_stream, uint32_t ret_num){
     // x_df -> x_code decode frame 
     incrementalDecode(x_df, adc_stream, ret_num); 
 
 }
 
-
+/**
+ * @brief ADC call back function for when x(not sure of the number) number of conversions has been done
+ * 
+ * @param handle 
+ * @param edata 
+ * @param user_data 
+ * @return true 
+ * @return false 
+ */
 static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data)
 {
     BaseType_t mustYield = pdFALSE;
@@ -67,6 +71,13 @@ static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_c
     return (mustYield == pdTRUE);
 }
 
+/**
+ * @brief Initialize the ADC to work in continuous mode
+ * 
+ * @param channel : 
+ * @param channel_num 
+ * @param out_handle 
+ */
 static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc_continuous_handle_t *out_handle)
 {
     adc_continuous_handle_t handle = NULL;
@@ -117,9 +128,15 @@ static bool check_valid_data(const adc_digi_output_data_t *data)
 
 
 esp_err_t conv_start_successfully;
-TaskHandle_t button_start_task = NULL;
 
-void button_start_decode(decode_frame_t *decode_frame, adc_continuous_handle_t handle){
+
+/**
+ * @brief Decode task: activated by the isr callback of the 'start_decode' push button
+ * 
+ * @param decode_frame :Decode frame
+ * @param handle : ADC handle 
+ */
+void button_start_decode(decode_frame_t *decode_frame, adc_continuous_handle_t handle, uint8_t *result){
         while (1) {
             ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 
@@ -137,8 +154,13 @@ void button_start_decode(decode_frame_t *decode_frame, adc_continuous_handle_t h
     }
 
 
-void IRAM_ATTR scan_reset (void* arg){
+static bool IRAM_ATTR scan_button_isr_handler (void* arg){
 
+    BaseType_t mustYield = pdFALSE;
+    //Notify that the start_decode button has been pushed
+    //Todo: Handle debounce and quick presses
+    vTaskNotifyGiveFromISR(button_start_task_handle, &mustYield);
+    return (mustYield == pdTRUE);   
 }
 
 
@@ -161,13 +183,16 @@ void app_main(void)
     io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
     io_conf.intr_type = GPIO_INTR_NEGEDGE;
 
+    gpio_config(&io_conf); 
+
+    gpio_set_intr_type(SCAN_RESET_PIN, GPIO_INTR_POSEDGE);
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(SCAN_RESET_PIN, scan_button_isr_handler, NULL);
 
 
     /**
-     * @brief Bluetooth Low Energy initialization
-     * 
+     * @brief Bluetooth Low Energy framework initialization     * 
      */
-
 
     /* Initialize NVS. */
     ret = nvs_flash_init();
@@ -227,17 +252,15 @@ void app_main(void)
         ESP_LOGE(DIVX_BLE_TAG, "set local  MTU failed, error code = %x", local_mtu_ret);
     }
 
-    /**
-     * @brief ADC and Processing
-     * 
-     */
-
-    // esp_err_t ret;
+    // Initialize ADC call parameters
+    uint8_t result[READ_LEN] = {0};
     uint32_t ret_num = 0;
     memset(result, 0xcc, READ_LEN);
 
+    // Handle to current task
     s_task_handle = xTaskGetCurrentTaskHandle();
 
+    // ADC Initialization 
     adc_continuous_handle_t handle = NULL;
     continuous_adc_init(channel, sizeof(channel) / sizeof(adc_channel_t), &handle);
 
@@ -245,6 +268,7 @@ void app_main(void)
         .on_conv_done = s_conv_done_cb,
     };
 
+    // Decode Frame Initialization 
     decode_frame_t decode_frame; 
     x_initialize(&decode_frame);
 
@@ -252,9 +276,10 @@ void app_main(void)
     // ESP_ERROR_CHECK(adc_continuous_start(handle));
     // All calls to start the ADC must come from the start-scan button. 
 
+
     // ADC is started within this task and should be stopped by the main task after a decode
     // fails or is successful 
-    xTaskCreate(button_start_decode, "Start Decoding", 4096, NULL, 10, &button_start_task);
+    xTaskCreate(button_start_decode, "Start Decoding", 4096, NULL, 10, &button_start_task_handle);
 
     while(1) {
 
